dce13baf70c2f8ccce2153a81bf445354a232e77
Majid <mobini.majid@gmail.com>
2015-02-03 18:20:54 +0330

    test delete

--- homeworks/1- functions, lists and exception handling/solution/q2.py
+++
@@ -1,7 +0,0 @@
-# question 2 solution
-
-from q1 import avg
-
-for i in range(1, 1000):
-    if avg(i)>5:
-        print i
--- homeworks/1- functions, lists and exception handling/solution/q3.py
+++
@@ -1,9 +0,0 @@
-# question 3 solution
-
-from q1 import avg
-
-def compare(a, b):
-    if avg(a)>avg(b):
-        return 1
-    else:
-        return 0
