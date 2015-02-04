1b246f1ca716baff81712ce95c298851130f771e
Majid <mobini.majid@gmail.com>
2015-02-04 15:25:37 +0330

    add useful imports to smaple-codes

---
+++ sample-codes/useful_imports.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import kivy
+kivy.require('1.8.0')
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.config import Config
+from kivy.clock import Clock
+
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+
+from kivy.uix.popup import Popup
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition, WipeTransition, SlideTransition, SwapTransition
+
+from kivy.graphics import Color, Ellipse, Line, Rectangle, Point
+
+
