dbbd5dd24455d30343a07d66df582dd6abf577ef
Majid <mobini.majid@gmail.com>
2015-02-04 15:31:44 +0330

    add some imports

--- sample-codes/useful_imports.py
+++ sample-codes/useful_imports.py
@@ -7,11 +7,13 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.config import Config
 from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
 
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.anchorlayout import AnchorLayout
 
 from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
